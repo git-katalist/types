6658e39555a8196270a64e86d7fd89ec1b87433c
tsots <mosalatsotetsi@gmail.com>
2022-07-26 17:48:11 +0200

    Deploy

--- _next/static/h-OpxNjkJCud7WF2pg-UT/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1978fc1243b702cb.js"],"/_error":["static/chunks/pages/_error-b53b8f599aff85eb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
